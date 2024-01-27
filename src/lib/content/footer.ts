28aee9c28848b07715a25c75f9e8ac333af6b294
shahzaib Shahid <110985745+shahzaib-191@users.noreply.github.com>
2024-01-28 00:30:30 +0500

    Add files via upload

---
+++ src/lib/content/footer.ts
@@ -0,0 +1,6 @@
+import { FooterSectionType } from '@/lib/types/sections';
+
+export const footerSection: FooterSectionType = {
+    title: 'Design & Built by M.Shahzaib',
+    link: 'https://github.com/shahzaib-191/learn-modern-python',
+};
---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,39 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/shaibyrajpoot302',
+  instagram: 'https://www.instagram.com/sher_bhatti_1/',
+  twitter: 'https://twitter.com/RajpootShaiby',
+  github: 'https://github.com/shahzaib-191/learn-modern-python',
+  linkedin: 'https://www.linkedin.com/in/shahzaib-rajpoot-78a042252/',
+};
+
+export const author = {
+  name: 'M.Shahzaib ',
+  email: 'shahzaibrajpoot61931@gmail.com',
+};
+
+export const seoData = {
+  title: 'M.Shahzaib | Front-End Developer',
+  description:
+    'M.Shahzaib is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://www.youtube.com/@illmorg',
+  keywords: [
+    'Shahzaib',
+    'M.shahzaib',
+    '@Shahid',
+    'M.shahzaib Shahid',
+    'Portfolio',
+    'Shahzaib Portfolio ',
+    'M.Shahzaib Portfolio',
+  ],
+};
